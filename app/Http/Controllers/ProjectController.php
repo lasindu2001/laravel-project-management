ca3ac71245a991adae6ce3093414f5e7108b58a4
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-13 20:07:47 +0530

    add project filtering by name and status

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,10 +16,18 @@ class ProjectController extends Controller
     {
         $query = Project::query();
 
+        if (request("name")) {
+            $query->where("name", "like", "%" . request("name") . "%");
+        }
+        if (request("status")) {
+            $query->where("status", request("status"));
+        }
+
         $projects = $query->paginate(10)->onEachSide(1);
 
         return inertia("Project/Index", [
             "projects" => ProjectResource::collection($projects),
+            'queryParams' => request()->query() ?: null,
         ]);
     }
 
--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,27 @@
 import Pagination from '@/Components/Pagination'
+import SelectInput from '@/Components/SelectInput'
+import TextInput from '@/Components/TextInput'
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from '@/constants'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import { Head, Link } from '@inertiajs/react'
+import { Head, Link, router } from '@inertiajs/react'
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+    queryParams = queryParams || {};
+
+    const searchFieldChanged = (name, value) => {
+        if (value) {
+            queryParams[name] = value;
+        } else {
+            delete queryParams[name];
+        }
+        router.get(route("project.index"), queryParams);
+    }
+
+    const onKeyPress = (name, e) => {
+        if (e.key !== "Enter") return;
+        searchFieldChanged(name, e.target.value);
+    }
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -38,6 +56,41 @@ export default function Index({ auth, projects }) {
                                             <th className="px-3 py-3">Due Date</th>
                                             <th className="px-3 py-3">Created By</th>
                                             <th className="px-3 py-3">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3">
+                                                <TextInput
+                                                    className="w-full"
+                                                    defaultValue={queryParams.name}
+                                                    placeholder="Project Name"
+                                                    onBlur={(e) =>
+                                                        searchFieldChanged("name", e.target.value)
+                                                    }
+                                                    onKeyPress={(e) => onKeyPress("name", e)}
+                                                />
+                                            </th>
+                                            <th className="px-3 py-3">
+                                                <SelectInput
+                                                    className="w-full"
+                                                    defaultValue={queryParams.status}
+                                                    onChange={(e) =>
+                                                        searchFieldChanged("status", e.target.value)
+                                                    }
+                                                >
+                                                    <option value="">Select Status</option>
+                                                    <option value="pending">Pending</option>
+                                                    <option value="in_progress">In Progress</option>
+                                                    <option value="completed">Completed</option>
+                                                </SelectInput>
+                                            </th>
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3"></th>
+                                            <th className="px-3 py-3"></th>
                                         </tr>
                                     </thead>
                                     <tbody>
