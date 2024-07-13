cf3cd6ec52d7a80d7c8ff65586dd729cde7aba5d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-13 22:02:56 +0530

    add project sorting by different properties

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,6 +16,9 @@ class ProjectController extends Controller
     {
         $query = Project::query();
 
+        $sortField = request("sort_field", 'created_at');
+        $sortDirection = request("sort_direction", "desc");
+
         if (request("name")) {
             $query->where("name", "like", "%" . request("name") . "%");
         }
@@ -23,7 +26,10 @@ class ProjectController extends Controller
             $query->where("status", request("status"));
         }
 
-        $projects = $query->paginate(10)->onEachSide(1);
+        $projects = $query
+            ->orderBy($sortField, $sortDirection)
+            ->paginate(10)
+            ->onEachSide(1);
 
         return inertia("Project/Index", [
             "projects" => ProjectResource::collection($projects),
--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -22,6 +22,20 @@ export default function Index({ auth, projects, queryParams = null }) {
         searchFieldChanged(name, e.target.value);
     }
 
+    const sortChanged = (name) => {
+        if (name === queryParams.sort_field) {
+            if (queryParams.sort_direction === "asc") {
+                queryParams.sort_direction = "desc";
+            } else {
+                queryParams.sort_direction = "asc";
+            }
+        } else {
+            queryParams.sort_field = name;
+            queryParams.sort_direction = "asc";
+        }
+        router.get(route("project.index"), queryParams);
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -48,12 +62,12 @@ export default function Index({ auth, projects, queryParams = null }) {
                                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                                         <tr className="text-nowrap">
-                                            <th className="px-3 py-3">ID</th>
+                                            <th onClick={(e) => sortChanged('id')} className="px-3 py-3">ID</th>
                                             <th className="px-3 py-3">Image</th>
-                                            <th className="px-3 py-3">Name</th>
-                                            <th className="px-3 py-3">Status</th>
-                                            <th className="px-3 py-3">Created Date</th>
-                                            <th className="px-3 py-3">Due Date</th>
+                                            <th onClick={(e) => sortChanged('name')} className="px-3 py-3">Name</th>
+                                            <th onClick={(e) => sortChanged('status')} className="px-3 py-3">Status</th>
+                                            <th onClick={(e) => sortChanged('created_at')} className="px-3 py-3">Created Date</th>
+                                            <th onClick={(e) => sortChanged('due_date')} className="px-3 py-3">Due Date</th>
                                             <th className="px-3 py-3">Created By</th>
                                             <th className="px-3 py-3">Actions</th>
                                         </tr>
