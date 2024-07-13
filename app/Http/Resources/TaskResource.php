693bf6794076b66467d649dc424171c05aae8438
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-14 00:32:29 +0530

    render all tasks

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,19 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'image_path' => $this->image_path,
+            'project' => new ProjectResource($this->project),
+            'assignedUser' => $this->assignedUser ? new UserResource($this->assignedUser) : null,
+            'createdBy' => new UserResource($this->createdBy),
+            'updatedBy' => new UserResource($this->updatedBy),
+        ];
     }
 }
