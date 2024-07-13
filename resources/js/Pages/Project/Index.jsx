ae1f80d75f56bcf3bf1e2a557af5719d3ac6556b
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-13 17:12:53 +0530

    render projects into a table

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -25,7 +25,63 @@ export default function Index({ auth, projects }) {
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="overflow-auto">
-                                {JSON.stringify(projects)}
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <th className="px-3 py-3">ID</th>
+                                            <th className="px-3 py-3">Image</th>
+                                            <th className="px-3 py-3">Name</th>
+                                            <th className="px-3 py-3">Status</th>
+                                            <th className="px-3 py-3">Created Date</th>
+                                            <th className="px-3 py-3">Due Date</th>
+                                            <th className="px-3 py-3">Created By</th>
+                                            <th className="px-3 py-3">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {projects.data.map((project) => (
+                                            <tr
+                                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                                key={project.id}
+                                            >
+                                                <td className="px-3 py-2">{project.id}</td>
+                                                <td className="px-3 py-2">
+                                                    <img src={project.image_path} style={{ width: 60 }} />
+                                                </td>
+                                                <th className="px-3 py-2 text-gray-100 text-nowrap hover:underline">
+                                                    <Link href={route("project.show", project.id)}>
+                                                        {project.name}
+                                                    </Link>
+                                                </th>
+                                                <td className="px-3 py-2">
+                                                    <span className="px-2 py-1 rounded text-white">
+                                                        {project.status}
+                                                    </span>
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    {project.created_at}
+                                                </td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    {project.due_date}
+                                                </td>
+                                                <td className="px-3 py-2">{project.createdBy.name}</td>
+                                                <td className="px-3 py-2 text-nowrap">
+                                                    <Link
+                                                        href={route("project.edit", project.id)}
+                                                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                    <button
+                                                        className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                    >
+                                                        Delete
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
