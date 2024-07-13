2a7bb6147c14317c02a81d4d2fac94d970d9e583
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-13 14:50:42 +0530

    database operations - task and project models, factories, seeding and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'id' => 1,
+            'name' => 'Lasindu',
+            'email' => 'lasindumvp2001@gmail.com',
+            'password' => bcrypt('qwer123.'),
+            'email_verified_at' => time()
         ]);
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
     }
 }
